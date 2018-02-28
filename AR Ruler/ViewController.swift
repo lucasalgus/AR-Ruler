3107bc6cdfb0630a49835b415d759ac6cc393f9f
Lucas Alves Gusmão <lucas.alvesx20013@gmail.com>
2018-02-28 19:55:10 -0300

    AR measuring features added.

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -13,21 +13,16 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    
+	
+	var textNode = SCNNode()
+	var dotNodes = [SCNNode]()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
+		
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+		//sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+		
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,35 +41,96 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		
+		if dotNodes.count >= 2 {
+			for dot in dotNodes {
+				dot.removeFromParentNode()
+			}
+			
+			textNode.removeFromParentNode()
+			dotNodes = [SCNNode]()
+			
+		}
+		
+		if let touch = touches.first?.location(in: sceneView) {
+			let hitTestResults = sceneView.hitTest(touch, types: .featurePoint)
+			
+			if let hitResult = hitTestResults.first {
+				addDot(at: hitResult)
+			}
+		}
+	}
+	
+	func addDot(at hitResult: ARHitTestResult) {
+		
+		let dotGeometry = SCNSphere(radius: 0.005)
+		
+		let material = SCNMaterial()
+		
+		material.diffuse.contents = UIColor.red
+		
+		dotGeometry.materials = [material]
+		
+		let node = SCNNode()
+		
+		node.position = SCNVector3(
+			x: hitResult.worldTransform.columns.3.x,
+			y: hitResult.worldTransform.columns.3.y,
+			z: hitResult.worldTransform.columns.3.z
+		)
+		
+		node.geometry = dotGeometry
+		
+		dotNodes.append(node)
+		
+		sceneView.scene.rootNode.addChildNode(node)
+		
+		if dotNodes.count >= 2 {
+			calculate()
+		}
+	}
+	
+	func calculate() {
+		let start = dotNodes[0]
+		let end = dotNodes[1]
+		
+		print(start.position)
+		print(end.position)
+		
+		let x = end.position.x - start.position.x
+		let y = end.position.y - start.position.y
+		let z = end.position.z - start.position.z
+		
+		let distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))
+		
+		updateText(text: "\(ceil(distance * 100))cm", at: end.position)
+	}
+	
+	func updateText(text: String, at position: SCNVector3) {
+		
+		textNode.removeFromParentNode()
+		
+		let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+		
+		textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+		
+		textNode = SCNNode(geometry: textGeometry)
+		
+		textNode.position = SCNVector3(position.x, position.y, position.z)
+		
+		textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+		
+		sceneView.scene.rootNode.addChildNode(textNode)
+	}
 }
+
+
+
+
+
+
+
+
+
